6ca77a7f8cac37d383a0c5c6bf0542a491943be3
mfndevil <71969249+mfndevil@users.noreply.github.com>
2023-02-14 19:43:08 +0000

    Milestone2-V2
    The if-else statements and while loops of the hangman game. Both if statements are in the while loop. The first if statement checks if the input from the user/player is a single character and if it is an alphabet. If both conditions are not met, then the game tells the user/player the input is invalid and a new input must be entered.  The second if statement checks if the input - called guess - is in the work from the list. If not, the game tells the user/player the input is incorrect. The while loop repeated if the input does not work for both if statements.

--- milestone_2.py
+++ milestone_2.py
@@ -1,27 +1,25 @@
 import random
-
-'list of words'
+# list of words
 word_list = ["mangoes", "pomegranates", "strawberry", "oranges", "pineapples"]
 word = random.choice(word_list)
+print(word) 
 
-print(word)
-
-'While loop to run continuously'
+# While loop to run continuously
 while True:
-    'gets user input'
+    # gets user input'
     guess = input("Please enter a single letter: ")
 
-    'checks if input is valid'
+    # checks if input is valid
     if guess.isalpha() and len(guess) == 1:
-        'Print if input is valid'
+        # Print if input is valid
         print("Input is valid")
         
     else:
-        'Print if input is not valid'
+        # Print if input is not valid
         print("Oops. Invalid input. Please enter a single alpabet")
         
 
-    'Checks if input letter is in the word from the list'
+    # Checks if input letter is in word
     if guess in word:
         print(f"Good. {guess} is in the word.")
     else:
